63c37d87f6ff29234170eb98ccbc71f226620564
rin4ik <m.mirakhmad@gmail.com>
2022-04-07 11:45:25 +0500

    first commit

---
+++ src/exercise/01.js
@@ -0,0 +1,28 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+function countReducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {count: state.count + action.step}
+        case 'DECREMENT':
+            return {count: state.count - action.step}
+        default:
+            throw new Error(`Unsupported aciton type: ${action.type}`);
+    }
+}
+function Counter({initialCount = 0, step = 3}) {
+    const [state, dispatch] = React.useReducer(countReducer, {
+        count: initialCount,
+      })
+      const {count} = state
+      const increment = () => dispatch({type: 'INCREMENT', step})
+    return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
